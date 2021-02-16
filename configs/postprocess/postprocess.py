f4f664d3f2d6d26aa851475027980a8134ac11f5
beaver48 <wwwinokur@gmail.com>
2021-02-16 15:59:23 +0300

    fix config

--- configs/postprocess/postprocess.py
+++ configs/postprocess/postprocess.py
@@ -1,16 +1,11 @@
 config = dict(
     detection_models=[
-        #dict(
+        # dict(
         #    type='mmdet',
         #    model='results/models/detectors_equalize/latest.pth',
         #    config='configs/mmdetection/detectors_training.py',
         #    num_gpu=2),
-        dict(
-            type='scaled_yolo',
-            model='runs2/exp15/weights/best.pt',
-            config='configs/yolo/data.yaml',
-            img_shape=1024
-        )
+        dict(type='scaled_yolo', model='runs2/exp15/weights/best.pt', config='configs/yolo/data.yaml', img_shape=1024)
     ],
     meta_file_path='data/processed/vinbigdataVOC2012/train.csv',
     test_meta_file_path='data/processed/vinbigdataVOC2012/test.csv',
